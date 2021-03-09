1bf850b9270780d62cad3380631455e6432135b1
Romain DESSART <romaindessart@gmail.com>
2021-03-10 00:16:00 +0100

    loading data from JSON

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from openap import Thrust, WRAP
 
 
 def main():
-    aircraft = json.loads(open("a320.json", 'r').read())
+    aircraft = json.loads(open("./data/a319.json", 'r').read())
     print(aircraft)
 
+
+if __name__ == "__main__":
+    main()
