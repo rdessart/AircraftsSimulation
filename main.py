caa75db1b24740adbd180635750cabd3e0de7b3c
Romain DESSART <romaindessart@gmail.com>
2021-03-10 19:37:53 +0100

    PEP8

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from openap import WRAP, aero
-
+import matplotlib.pyplot as plt
 import performance
+
 
 def main():
     a319 = performance.Performance("A319")
@@ -14,50 +15,58 @@ def main():
     a319.gear = False
     a319.flaps = 1
     a319.pitch_target = 15.0
-    #simulation varibale
-    has_reduced_thrust = False
-    has_accelerated = False
+    # simulation varibale
     a319.phase = "TO_CLIMB"
     print("Starting")
-    while a319.altitude < 10_000.0 * aero.ft:
+    alts = []
+    vs = []
+    times = []
+    i = 0
+    while a319.altitude < 4_000.0 * aero.ft:
         a319.run()
-        print(a319.altitude / aero.ft)
-        #When passing 100ft RA, the gear is up
+        # When passing 100ft RA, the gear is up
         if (a319.altitude / aero.ft) > 100.0:
-            
             a319.gear = False
 
         if (a319.altitude / aero.ft) > 1500.0 and\
-           (a319.altitude / aero.ft) < 1550.0  and a319.phase != "THR_RED":
-            a319.thrust_percent = 0.8 # Thrust reduction altitud
+           (a319.altitude / aero.ft) < 1550.0 and a319.phase != "THR_RED":
+            a319.thrust_percent = 0.8  # Thrust reduction altitud
             a319.phase = "THR_RED"
 
         if (a319.altitude / aero.ft) > 3000.0:
+            alts.append(int(round(a319.altitude / aero.kts)))
+            vs.append(int(a319.vs))
             if a319.phase != "ACC":
+                i = 0
                 a319.pitch_target = 10.0
                 a319.phase = "ACC"
             if a319.flaps > 0 and (a319.cas / aero.kts) > 210.0:
                 a319.flaps = 0
-            if a319.vs > 1000.0 and a319.v_y > 0.5 and round(a319.pitch) == a319.pitch_target:
+            if a319.vs > 1000.0 and round(a319.pitch) == a319.pitch_target:
                 a319.pitch_target -= 1
-            elif a319.vs < 1000.0 and a319.v_y < 0.5 and round(a319.pitch) == a319.pitch_target:
+            elif a319.vs < 1000.0 and round(a319.pitch) == a319.pitch_target:
                 a319.pitch_target += 1
-        
-        if a319.flaps == 0 and round(a319.cas / aero.kts) == 250:
-            a319.phase = "CLIMB_1"
-            if a319.pitch == a319.pitch_target:
-                if a319.acc_x > 0:
-                    a319.pitch_target += 1
-                elif a319.acc_x < 0:
-                    a319.pitch_target -=1
+            i += 1
+            times.append(i / 60.0)
 
+        # if a319.flaps == 0 and round(a319.cas / aero.kts) == 250:
+        #     a319.phase = "CLIMB_1"
+        #     if a319.pitch == a319.pitch_target:
+        #         if a319.acc_x > 0:
+        #             a319.pitch_target += 1
+        #         elif a319.acc_x < 0:
+        #             a319.pitch_target -=1
 
     print(f"Cas Start  = {cas_start / aero.kts}")
     print(f"Cas End  = {a319.cas / aero.kts}")
     print("Finished")
-    
+    fig, ax = plt.subplots()
+    ax.plot(times, vs)
+    ax.plot(times, alts)
+    plt.axhline(1000.0)
+    plt.grid()
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
-
-# 92 -> 87 || 178.84 -> 169
