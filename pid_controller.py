db4fac328899f12f236909c739bc5e77ea37d3da
Romain DESSART <romaindessart@gmail.com>
2021-03-12 09:43:23 +0100

    added PIDv2

--- pid_controller.py
+++ pid_controller.py
@@ -48,3 +48,84 @@ class PIDController():
 
     def get_kie(self):
         return self.ki[0] * self.integral_error
+
+
+class PIDController2():
+    """Updated version of PIDController"""
+    def __init__(self, Kp: float = 0.0, Ki: float = 0.0, Kd: float = 0.0,
+                 limitMin: float = 0.0, limitMax: float = 0.0,
+                 tau: float = 0.0, dp: float = 0.0):
+        """Version 2 of PIDController, with updated math
+        Args:
+            Kp (float, optional): gain for proportional. Defaults to 0.0.
+            Ki (float, optional): gain for integrator. Defaults to 0.0.
+            Kd (float, optional): gain for derivator. Defaults to 0.0.
+            limitMin (float, optional): Max Output value. Defaults to 0.0.
+            limitMax (float, optional): Min Output value. Defaults to 0.0.
+            tau (float, optional): gain for low pass filter. Defaults to 0.0.
+            dp (float, optional): timestep. Defaults to 0.0.
+        """
+        self.kp = Kp
+        self.ki = Ki
+        self.kd = Kd
+        self.limitMax = limitMax
+        self.limitMin = limitMin
+        self.tau = tau
+        self.dp = dp
+        self.use_low_pass_filter = False
+        self.use_kick_avoidance = False
+
+        self._integrator = 0.0
+        self._differenciator = 0.0
+        self._previousError = 0.0
+        self._previousMeasurement = 0.0
+
+    def compute(self, target: float, 
+                measurement: float, dt: float = None) -> float:
+        """Calculate output through PID interface using gain value set via
+        self.Kp, self.Ki, self.Kd 
+
+        Args:
+            target (float): target value to be reached
+            measurement (float): actual value
+            dt (float, optional): Timestep since the las call. if default we
+                                 uses self.dt. Defaults to None.
+
+        Returns:
+            float: The output of the PID clamped to 
+                   [self.limitMax, self.limitMax]
+        """
+        if dt is None:
+            dt = self.dt
+        limitIntMax = 0.0
+        limitIntMin = 0.0
+        error = target - measurement
+        proportional = error * self.kp
+        self._integrator += 0.5 * self.ki * dt * (error + self._previousError)
+        if self.limitMax > self._integrator:
+            limitIntMax = self.limitMax - proportional
+        elif self.limitMin < self._integrator:
+            limitIntMin = self.limitMin - proportional
+        
+        if self._integrator > self.limitMax:
+            self._integrator = self.limitMax
+        elif self._integrator < self.limitMin:
+            self._integrator = self.limitMin
+        
+        if self.use_kick_avoidance:
+            differenciator_delta = measurement - self._previousMeasurement
+        else:
+            differenciator_delta = error - self._previousError
+        self._differenciator = (2.0 * self.Kd * differenciator_delta)
+        if self.use_low_pass_filter:
+            self._differenciator += ((2.0 * self.tau * dt)
+                                    * self._differenciator)\
+                                 / (2.0 * self.tau * dt)
+        output = proportional + self._integrator + self._differenciator
+        if output > self.limitMax:
+            output = self.limitMax
+        elif output < self.limitMin:
+            output =  self.limitMin
+        self._previousError = error
+        self._previousMeasurement = measurement
+        return output
