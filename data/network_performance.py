321f9f83ac8f3a8eecaff8af84c5c6693bf5e1c2
Romain DESSART <romaindessart@gmail.com>
2021-03-25 20:39:07 +0100

    Added thrust and drag tracjing

--- data/network_performance.py
+++ data/network_performance.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 list_aoas = []
 list_cl = []
 list_cd = []
+list_alt = []
+mach_thrust = {}
 aoas = {}
 
 
@@ -15,12 +17,16 @@ def main():
             data = soc.recvfrom(1024, 0)
         except KeyboardInterrupt:
             return
-        aoa, cl, cd = data[0].decode().strip('|').split(',')
+        perfo = data[0].decode().strip('|')
+        aoa, cl, cd, mach, altitude, thrust = perfo.split(',')
         aoa = round(float(aoa), 2)
         cl = float(cl)
         cd = float(cd)
+        mach = round(float(mach), 3)
+        altitude = round(float(altitude))
+        thrust = round(float(thrust))
         aoas[aoa] = [cl, cd]
-        print(f"{float(aoa)} - {float(cl)} - {float(cd)}")
+        print(f"{aoa} - {cl} - {cd} - {mach} - {altitude} - {thrust}")
         if aoa not in list_aoas:
             list_aoas.append(aoa)
             list_cd.append(cd)
@@ -30,10 +36,13 @@ def main():
             list_cd[index] = cd
             list_cl[index] = cl
 
+        mach_thrust[mach] = thrust
+
 
 if __name__ == "__main__":
     main()
     fout = open("a319_ld.csv", 'w+')
+    
     fout.write("AOA,Cl,Cd\n")
     keys = [aoa for aoa in aoas.keys()]
     keys.sort()
@@ -42,8 +51,26 @@ if __name__ == "__main__":
     for key in keys:
         fout.write(f"{key},{aoas[key][0]},{aoas[key][1]}\n")
         fout.flush()
+    fout.close()
+    fout2 = open("a319_thrust_40k.csv", 'w+')
+    fout2.write("Mach,Thrust\n")
+    keys = [mach for mach in mach_thrust.keys()]
+    keys.sort()
+    machs = []
+    thrusts = []
+    for key in keys:
+        fout2.write(f"{key},{mach_thrust[key]}\n")
+        fout2.flush()
+        machs.append(key)
+        thrusts.append(mach_thrust[key])
+    fout2.close()
+
     fig, ax = plt.subplots()
     plt.plot(list_aoas, list_cd)
     plt.plot(list_aoas, list_cl)
-    ax.legend(["Cl", "Cd"])
+    ax.legend(["Cd", "Cl"])
+    plt.show()
+    fig2, ax2 = plt.subplots()
+    plt.plot(machs, thrusts)
+    ax2.legend(["Thrust"])
     plt.show()
